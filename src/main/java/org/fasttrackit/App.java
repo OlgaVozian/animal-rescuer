7e9d8d766c3e323bcb21e6d63e3c375c027157d0
OlgaVozian <olga.vozian@gmail.com>
2020-05-04 20:42:44 +0300

    set-up basic project structure

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,40 @@
 package org.fasttrackit;
-
-/**
- * Hello world!
- *
- */
+import java.time.LocalDateTime;
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Animal animal = new Animal();
+        animal.age = 3;
+        animal.levelOfFeelingHungry = 5;
+        animal.levelOfHealth = 10;
+        animal.moodLevel = 10;
+        animal.name = "Narta";
+        animal.nameOfFavoriteFood ="Potatoes";
+        animal.preferredRecreationalActivity = "Panda friend";
+
+        Adopter adopter = new Adopter();
+        adopter.availableMoney = 789456;
+        adopter.name = "Olga Vozian";
+
+        PetFood petFood = new PetFood();
+        petFood.availabilityInStock = 456789123;
+        petFood.expirationDate = LocalDateTime.of(2022,02, 11,9,15,45);
+        petFood.name = "Pedigree";
+        petFood.price = (float) 25.12;
+        petFood.quantity = 789.26;
+
+        RecreationActivity recreactionActivity = new RecreationActivity();
+        recreactionActivity.name ="jumps";
+
+        Veterinarian veterinarian = new Veterinarian();
+        veterinarian.name = "Mihail Vozian";
+        veterinarian.specialization = "dogs";
+
+        Game game = new Game();
+        game.adopter = "Olga Vozian";
+        game.dog = "Narta";
+        game.veterinarian = "Mihail Vozian";
+
     }
 }
